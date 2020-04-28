5d87913e874051ca87035941f91dd9e0cec0be34
Glenn Sidle <gsidle.math@gmail.com>
2020-04-28 07:38:14 -0400

    do board setup in js, have pieces draggable to new space

--- shobuMain.js
+++ shobuMain.js
@@ -12,10 +12,37 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
+function setUpBoards() {
+    var b;
+    let board_html = "";
+    for (b = 0; b < 4; b ++){
+        let bc = ""
+        if (b == 0 || b == 2) {
+            bc = "#B37700";
+        }
+        else {
+            bc = "#664400";
+        }
+        let board_str = "<div class='board' style='background-color: " + bc + "' id='board" + b.toString() + "'>";
+        board_html += board_str;
+        var i;
+        var j;
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                let id_str = b.toString() + i.toString() + j.toString();
+                let space_str = "<div class='space' id='" + id_str + "' ondrop='drop(event)' ondragover='allowDrop(event)'></div>";
+                board_html += space_str;
+            }
+        }
+        board_html += "</div>";
+    }
+    document.getElementById("main").innerHTML = board_html;
+}
+
 function playAs(color){
     let wp1 = "<div class='piece' draggable='true' ondragstart='drag(event)' style='background-color: #DDD'";
     let wp2 = "></div>";
-    let bp1 = "<div class='piece' style='background-color: #111'";
+    let bp1 = "<div class='piece' draggable='true' ondragstart='drag(event)' style='background-color: #111'";
     let bp2 = "></div>";
     if (color == "white") {
         var b;
@@ -46,3 +73,5 @@ function playAs(color){
         }
     }
 }
+
+setUpBoards();
