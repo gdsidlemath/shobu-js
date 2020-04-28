ffd4fd771e36b71a8774857d07da12d9d98e8e3f
Glenn Sidle <gsidle.math@gmail.com>
2020-04-28 15:13:45 -0400

    condensing code, restricting movement

--- shobuMain.js
+++ shobuMain.js
@@ -1,80 +1,133 @@
-function allowDrop(ev) {
-    ev.preventDefault();
+document.addEventListener("dragstart", function(event) {
+    event.dataTransfer.setData("text", event.target.id);
+});
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    var piece = document.getElementById(event.dataTransfer.getData("text"));
+    var space = document.getElementById(event.target.id);
+    movePiece(piece, space);
+});
+
+function checkMove(px, py, sx, sy) {
+    let diff_x = Math.abs(sx - px);
+    let diff_y = Math.abs(sy - py);
+    if ( diff_x == diff_y && diff_x < 3 ) {
+        return true;
+    }
+    else if ( diff_x < 3 && diff_y == 0 ) {
+        return true;
+    }
+    else if ( diff_x == 0 && diff_y < 3 ) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    // make sure the piece stays on the correct board!
-    if (ev.target.id.substr(0,1) == data.substr(3, 1)) {
-        ev.target.appendChild(document.getElementById(data));
+function movePiece(piece, space) {
+    if ( ( whiteMove && piece.player_num == 1 ) || ( blackMove && piece.player_num == 0 ) ) {
+        let board_condition = ( space.board_number == piece.board_number );
+        let empty_condition = ( space.className === "space" );
+        let opponent_condition = false;
+        if ( !empty_condition ) {
+            opponent_condition = space.player_num - piece.player_num != 0;
+        }
+        let move_condition = checkMove(piece.x, piece.y, space.x, space.y);
+        if ( board_condition && ( empty_condition || opponent_condition ) && move_condition ) {
+            piece.x = space.x;
+            piece.y = space.y;
+            space.stone = piece;
+            if ( space.className === "space" ){
+                space.appendChild(piece);
+            }
+            else {
+                let actual_space = space.parentNode;
+                actual_space.removeChild(space);
+                actual_space.appendChild(piece);
+            }
+        }
     }
 }
 
 function setUpBoards() {
+    let main = document.getElementById("main");
+    if ( main.childNodes.length > 1 ) {
+        var b;
+        for (b = 0; b < 4; b++) {
+           let board = document.getElementById("board" + b.toString());
+           main.removeChild(board);
+        }
+    }
     var b;
-    let board_html = "";
     for (b = 0; b < 4; b ++){
-        let bc = ""
+        let board = document.createElement("div");
+        board.board_number = b;
+        board.id = "board" + b.toString();
+        board.className = "board";
         if (b == 0 || b == 2) {
-            bc = "#B37700";
+            board.style.cssText = "background-color:#B37700";
         }
         else {
-            bc = "#664400";
+            board.style.cssText = "background-color:#664400";
         }
-        let board_str = "<div class='board' style='background-color: " + bc + "' id='board" + b.toString() + "'>";
-        board_html += board_str;
         var i;
         var j;
         for (i = 0; i < 4; i++) {
             for (j = 0; j < 4; j++) {
-                let id_str = b.toString() + i.toString() + j.toString();
-                let space_str = "<div class='space' id='" + id_str + "' ondrop='drop(event)' ondragover='allowDrop(event)'></div>";
-                board_html += space_str;
+                let space = document.createElement("div");
+                space.board_number = b;
+                space.x = i;
+                space.y = j;
+                space.className = "space";
+                space.id = "space" + b.toString() + i.toString() + j.toString();
+                space.stone = null;
+                board.appendChild(space);
             }
         }
-        board_html += "</div>";
+    document.getElementById("main").appendChild(board);
     }
-    document.getElementById("main").innerHTML = board_html;
+}
+
+function initPiece(player_num, color, b, px, py) {
+    let piece = document.createElement("div");
+    piece.className = "piece";
+    piece.id = "piece" + player_num.toString() + py.toString() + "board" + b.toString();
+    piece.style.cssText = "background-color:#" + color;
+    piece.board_number = b;
+    piece.x = px;
+    piece.y = py;
+    piece.draggable = true;
+    piece.player_num = player_num;
+    let space_id = "space" + b.toString() + px.toString() + py.toString();
+    let space = document.getElementById(space_id);
+    space.appendChild(piece);
+    space.stone = piece;
 }
 
 function playAs(color){
-    let wp1 = "<div class='piece' draggable='true' ondragstart='drag(event)' style='background-color: #DDD'";
-    let wp2 = "></div>";
-    let bp1 = "<div class='piece' draggable='true' ondragstart='drag(event)' style='background-color: #111'";
-    let bp2 = "></div>";
-    if (color == "white") {
-        var b;
-        for (b = 0; b < 4; b++) {
-            var i;
-            for (i = 0; i < 4; i ++) {
-                let w_s_str = b.toString() + "3" + i.toString();
-                let w_p_str = "id=wp_" + b.toString() + i.toString();
-                document.getElementById(w_s_str).innerHTML = wp1 + w_p_str + wp2;
-                let b_s_str = b.toString() + "0" + i.toString();
-                let b_p_str = "id=bp_" + b.toString() + i.toString();
-                document.getElementById(b_s_str).innerHTML = bp1 + b_p_str + bp2;
+    setUpBoards();
+    var b;
+    for (b = 0; b < 4; b++) {
+        var j;
+        for (j = 0; j < 4; j ++) {
+            if (color == "white") {
+                initPiece(1, "DDD", b, 3, j);
+                initPiece(0, "111", b, 0, j);
             }
-        }
-    }
-    if (color == "black") {
-        var b;
-        for (b = 0; b < 4; b++) {
-            var i;
-            for (i = 0; i < 4; i ++) {
-                let w_s_str = b.toString() + "0" + i.toString();
-                let w_p_str = "id=wp_" + b.toString() + i.toString();
-                document.getElementById(w_s_str).innerHTML = wp1 + w_p_str + wp2;
-                let b_s_str = b.toString() + "3" + i.toString();
-                let b_p_str = "id=bp_" + b.toString() + i.toString();
-                document.getElementById(b_s_str).innerHTML = bp1 + b_p_str + bp2;
+            else {
+                initPiece(1, "DDD", b, 0, j);
+                initPiece(0, "111", b, 3, j);
             }
         }
     }
 }
 
+let whiteMove = false;
+let blackMove = false;
 setUpBoards();
